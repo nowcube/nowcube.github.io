2b7dd0dcb6e83101238fc276860e10d08ee35fba
moeday <fguning@qq.com>
2022-08-03 17:02:11 +0800

    fix navbar & add wallpaper

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
+//用js设置navbar伸缩
 var displayMenu = document.getElementById("show_menu");
 var menu_status = 1;
-window.onresize = function(){
-    if(document.documentElement.clientWidth > 576){
-        document.getElementById("item").style.display = "flex";
-        document.getElementById("item2").style.display = "flex";
-        document.getElementById("item3").style.display = "flex";
-        document.getElementById("item4").style.display = "flex";
-        // document.getElementById("navbar").style.height = "50px";
-    }else{
-        document.getElementById("item").style.display = "none"; 
-        document.getElementById("item2").style.display = "none"; 
-        document.getElementById("item3").style.display = "none"; 
-        document.getElementById("item4").style.display = "none"; 
-        // document.getElementById("navbar").style.height = "50px";
-    }
-}
 displayMenu.onclick = function () {
     if (menu_status == 1) {
         document.getElementById("item").style.display = "flex";
@@ -24,11 +10,22 @@ displayMenu.onclick = function () {
         document.getElementById("navbar").style.height = "auto";
         menu_status = 2;
     } else if (menu_status == 2) {
-        document.getElementById("item").style.display = "none"; 
-        document.getElementById("item2").style.display = "none"; 
-        document.getElementById("item3").style.display = "none"; 
-        document.getElementById("item4").style.display = "none"; 
-        document.getElementById("navbar").style.height = "50px";
+        document.getElementById("item").style.display = ""; 
+        document.getElementById("item2").style.display = ""; 
+        document.getElementById("item3").style.display = ""; 
+        document.getElementById("item4").style.display = ""; 
+        document.getElementById("navbar").style.height = "";
         menu_status = 1;
     }
 }
+
+//设置body背景
+var htmlBgs=[];
+htmlBgs[0]="./bg.jpg";
+htmlBgs[1]="./bg2.jpg";
+htmlBgs[2]="./bg3.jpg";
+htmlBgs[3]="./bg4.jpg";
+htmlBgs[4]="./bg5.jpg";
+
+var randomBgIndex = Math.round(Math.random()*4);
+document.body.style.backgroundImage="url('"+htmlBgs[randomBgIndex]+"')";
